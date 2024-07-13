ee3a777e141ed268dc2456b631720268bc18a5fe
Chen Yulin <chenyulin@sjtu.edu.cn>
2024-07-14 01:38:09 +0800

    feat. init

---
+++ index.js
@@ -0,0 +1,42 @@
+hexo.extend.filter.register('before_generate', () => {
+  console.log('This is my custom Hexo plugin!');
+});
+hexo.extend.filter.register('before_post_render', function(data) {
+  const startTag = '--picture_wall--';
+  const endTag = '--picture_wall_end--';
+
+  const regex = new RegExp(`${startTag}[\\s\\S]*?${endTag}`, 'g');
+
+  data.content = data.content.replace(regex, match => {
+    const pictures = match
+      .replace(startTag, '')
+      .replace(endTag, '')
+      .trim();
+
+    const pictureArray = pictures.split('\n').map(item => item.trim()).filter(item => item);
+
+    pictureArray.forEach(picture => {
+        const srcMatch = picture.match(/!\[.*?\]\((.*?)\s+\"(.*?)\"\)/);
+        if (srcMatch && srcMatch[1]) {
+        console.log(`Found image: ${srcMatch[1]}, title: ${srcMatch[2]}`);  // 输出匹配到的图片路径和标题
+        }
+    });
+
+    let pictureWallHtml = '<div class="picture-wall">';
+
+    pictureArray.forEach(picture => {
+      const srcMatch = picture.match(/!\[.*?\]\((.*?)\)/);
+      if (srcMatch && srcMatch[1]) {
+        pictureWallHtml += `<div class="picture-wall-item"><img src="${srcMatch[1]}" alt=""></div>`;
+      }
+    });
+
+    pictureWallHtml += '</div>';
+
+    console.log(pictureWallHtml);
+
+    return pictureWallHtml;
+  });
+
+  return data;
+});
